78491a8efe25a25b9ae42ff58c5fa3c24e1b8081
D-Fink <dfink4badjuju@gmail.com>
2020-04-30 17:38:54 -0600

    asdfa

--- adventure/api.py
+++ adventure/api.py
@@ -72,6 +72,6 @@ def say(request):
 # type of request
 @api_view(["GET"])
 def rooms(request):
-    world = World.objects.all().values()
+    world = World.all().values()
     room = Room.objects.all().values()
     return JsonResponse({'title': list(room), 'grid': list(world)}, safe=True)
